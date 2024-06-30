13daefced5d6ddb275721bf3a752a0b3b53cd277
wlsdks <wlsdks12@naver.com>
2024-06-30 17:05:27 +0900

    [PULC-1] :sparkles: 앞으로 사용하게 될 서비스 레이어 미리 작성

---
+++ src/main/java/com/pulse/content/service/ContentServiceImpl.java
@@ -0,0 +1,15 @@
+package com.pulse.content.service;
+
+import com.pulse.content.service.usecase.ContentService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Slf4j
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+@Service
+public class ContentServiceImpl implements ContentService {
+
+}

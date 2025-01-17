a05c47852e6de6cc6638677c7633273b01fdc268
dabin0513 <dabin0513@naver.com>
2025-01-17 20:32:28 +0900

    [MP-30] :recycle: 엔티티 리팩토링

--- src/main/java/com/pulse/content/adapter/out/persistence/entity/CommentEntity.java
+++ src/main/java/com/pulse/content/adapter/out/persistence/entity/CommentEntity.java
@@ -6,17 +6,41 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.util.Objects;
+
 @Getter
 @Entity
 @Table(name = "comment")
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class CommentEntity extends BaseEntity {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "post_id")
     private Long postId;    // 게시글 id
+
+    @Column(name = "member_id")
     private Long memberId;  // 작성자 회원 id
+
+    @Column(name = "title")
     private String title;   // 게시글 내용 제목
+
+    @Column(name = "text")
     private String text;    // 게시글 내용 텍스트
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        CommentEntity that = (CommentEntity) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
--- src/main/java/com/pulse/content/adapter/out/persistence/entity/HashTagEntity.java
+++ src/main/java/com/pulse/content/adapter/out/persistence/entity/HashTagEntity.java
@@ -6,14 +6,32 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.util.Objects;
+
 @Getter
 @Entity
 @Table(name = "hashtag")
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class HashTagEntity extends BaseEntity {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "name")
     private String name;    // 해시태그 이름
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        HashTagEntity that = (HashTagEntity) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }

80dd99d6493381a47fa195e756b3dd766de693e0
dabin0513 <dabin0513@naver.com>
2025-01-30 21:01:29 +0900

    [MP-34] :recycle: 도메인 리팩토링

--- src/main/java/com/pulse/content/domain/Report.java
+++ src/main/java/com/pulse/content/domain/Report.java
@@ -2,6 +2,7 @@ package com.pulse.content.domain;
 
 import com.pulse.content.adapter.out.persistence.entity.TargetType;
 import com.pulse.content.domain.key.CategoryId;
+import com.pulse.content.domain.key.MemberId;
 import com.pulse.content.domain.key.ReportId;
 import lombok.*;
 
@@ -15,16 +16,16 @@ public class Report {
 
     private TargetType targetType;  // 신고 대상 타입(POST, COMMENT)
 
-    private Long targetId;          // 신고 대상 id
+    private MemberId targetId;          // 신고 대상 id
 
-    private Long memberId;          // 신고한 회원 id
+    private MemberId memberId;          // 신고한 회원 id
 
     private CategoryId reportCategoryId;  // 신고 카테고리 id
 
     private String reason;          // 신고 사유 내용
 
     // factory method
-    public static Report of(ReportId reportId, TargetType targetType, Long targetId, Long memberId, CategoryId reportCategoryId, String reason) {
+    public static Report of(ReportId reportId, TargetType targetType, MemberId targetId, MemberId memberId, CategoryId reportCategoryId, String reason) {
         return Report.builder()
                 .reportId(reportId)
                 .targetType(targetType)

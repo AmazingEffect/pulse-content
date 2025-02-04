31b77d4be8096e867751d2bee158223c03663458
chaesuhyeon <ddd8177@gmail.com>
2025-02-04 19:47:33 +0900

    [MP-35] :recycle: domain record -> class로 변경

--- src/main/java/com/pulse/content/domain/Member.java
+++ src/main/java/com/pulse/content/domain/Member.java
@@ -1,11 +1,15 @@
 package com.pulse.content.domain;
 
 import com.pulse.content.domain.key.MemberId;
+import lombok.*;
 
-public record Member(
-        MemberId memberId,
-        String name,
-        String nickName,
-        String profilePictureUrl
-) {
+@Getter
+@Builder(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class Member {
+    private MemberId memberId;
+    private String name;
+    private String nickName;
+    private String profilePictureUrl;
 }
--- src/main/java/com/pulse/content/domain/Reaction.java
+++ src/main/java/com/pulse/content/domain/Reaction.java
@@ -4,11 +4,16 @@ package com.pulse.content.domain;
 import com.pulse.content.adapter.out.persistence.entity.ReactionType;
 import com.pulse.content.adapter.out.persistence.entity.TargetType;
 import com.pulse.content.domain.key.ReactionId;
+import lombok.*;
 
-public record Reaction(
-        ReactionId reactionId,
-        TargetType targetType,
-        Long targetId,
-        Long memberId,
-        ReactionType reactionType
-) {}
+@Getter
+@Builder(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class Reaction {
+    private ReactionId reactionId;
+    private TargetType targetType;
+    private Long targetId;
+    private Long memberId;
+    private ReactionType reactionType;
+}

3eae610e96ce125e7d13a76b5edf5393156357a6
dabin0513 <dabin0513@naver.com>
2025-02-18 21:26:59 +0900

    [MP-44] :sparkles: CreateContent request&response DTO 추가

---
+++ src/main/java/com/pulse/content/adapter/in/web/dto/response/CreateContentResponseDTO.java
@@ -0,0 +1,28 @@
+package com.pulse.content.adapter.in.web.dto.response;
+
+import com.pulse.content.common.enumerate.PostStatus;
+import com.pulse.content.common.enumerate.PostVisibility;
+import com.pulse.content.domain.key.AttachId;
+import com.pulse.content.domain.key.FileId;
+import com.pulse.content.domain.key.MemberId;
+import com.pulse.content.domain.key.PostId;
+import lombok.*;
+
+@Getter
+@AllArgsConstructor
+@Builder(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class CreateContentResponseDTO {
+    private PostId postId;
+    private MemberId memberId;
+    private Long hashTagIds;
+    private Long categoryIds;
+    private AttachId attachId;
+    private String url;
+    private FileId fileId;
+    private String title;
+    private String text;
+    private PostStatus postStatus;
+    private PostVisibility postVisibility;
+
+}

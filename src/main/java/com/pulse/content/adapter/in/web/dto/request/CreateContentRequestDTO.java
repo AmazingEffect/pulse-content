4b4c4d8f30428c677a61d4d7021a21cebb435a98
dabin0513 <dabin0513@naver.com>
2025-02-22 17:57:09 +0900

    [MP-44] :sparkles: todo 작성

--- src/main/java/com/pulse/content/adapter/in/web/dto/request/CreateContentRequestDTO.java
+++ src/main/java/com/pulse/content/adapter/in/web/dto/request/CreateContentRequestDTO.java
@@ -3,26 +3,22 @@ package com.pulse.content.adapter.in.web.dto.request;
 import com.pulse.content.common.enumerate.PostStatus;
 import com.pulse.content.common.enumerate.PostVisibility;
 import com.pulse.content.domain.key.AttachId;
-import com.pulse.content.domain.key.FileId;
 import com.pulse.content.domain.key.MemberId;
-import com.pulse.content.domain.key.PostId;
 import lombok.*;
+
+import java.io.File;
+import java.util.List;
 
 @Getter
 @AllArgsConstructor
 @Builder(access = AccessLevel.PRIVATE)
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class CreateContentRequestDTO {
-    private PostId postId;
-    private MemberId memberId;
-    private Long hashTagIds;
-    private Long categoryIds;
-    private AttachId attachId;
-    private String url;
-    private FileId fileId;
-    private String title;
-    private String text;
-    private PostStatus postStatus;
-    private PostVisibility postVisibility;
-
+    private MemberId memberId;                  // 작성자 id
+    private List<String> hashTagNames;          // hashTag 목록
+    private List<Long> categoryIds;             // 카테고리 Id 목록
+    private List<File> fileId;                   // 첨부 파일 목록
+    private String title;                       // 게시글 제목
+    private String text;                        // 게시글 내용
+    private PostVisibility postVisibility;      // 공개 범위
 }
--- src/main/java/com/pulse/content/application/service/CreateContentService.java
+++ src/main/java/com/pulse/content/application/service/CreateContentService.java
@@ -20,9 +20,17 @@ public class CreateContentService implements CreateContentsUseCase {
     @Override
     @Transactional
     public CreateContentResponseDTO create(CreateContentRequestDTO createContentRequestDto) {
+        // todo: 1. hashTag 저장
+
+        // todo: 2. 게시글 상태(postStatus) 세팅
+
+        // todo: 3. dto -> domain 변환
+
+        // todo: 4. 게시글 저장
         Post post = contentMapper.createRequestDtoToDomain(createContentRequestDto);
         Post createdPost = createContentPort.create(post);
 
+        // todo: 5. 첨부 파일 저장(이벤트)
         return contentMapper.domainToCreateResponseDTO(createdPost);
     }
 }

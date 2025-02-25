89bf4d82422f9df8eb21195410d490fddfba50d7
dabin0513 <dabin0513@naver.com>
2025-02-25 21:02:35 +0900

    [MP-46] :sparkles: PostCategory 레포지토리 추가

---
+++ src/main/java/com/pulse/content/adapter/out/persistence/repository/mapping/PostCategoryRepository.java
@@ -0,0 +1,7 @@
+package com.pulse.content.adapter.out.persistence.repository.mapping;
+
+import com.pulse.content.adapter.out.persistence.entity.mapping.PostCategoryEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostCategoryRepository extends JpaRepository<PostCategoryEntity, Long> {
+}

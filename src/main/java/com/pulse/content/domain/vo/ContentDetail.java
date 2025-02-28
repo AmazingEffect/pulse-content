eb4fb671da37deb0d0ecf0f6f5ab31b176c905a5
dabin0513 <dabin0513@naver.com>
2025-02-28 20:55:21 +0900

    [MP-49] :recycle: 코드 리팩토링
    도메인 equals&hashcode 메서드 제거

--- src/main/java/com/pulse/content/domain/vo/ContentDetail.java
+++ src/main/java/com/pulse/content/domain/vo/ContentDetail.java
@@ -1,9 +1,6 @@
 package com.pulse.content.domain.vo;
 
-import com.pulse.content.adapter.out.persistence.entity.vo.ContentDetailEntity;
 import lombok.*;
-
-import java.util.Objects;
 
 @Getter
 @Builder(access = AccessLevel.PUBLIC)
@@ -20,18 +17,5 @@ public class ContentDetail {
                 .title(title)
                 .text(text)
                 .build();
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        ContentDetail that = (ContentDetail) o;
-        return Objects.equals(title, that.title) && Objects.equals(text, that.text);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(title, text);
     }
 }
--- src/main/java/com/pulse/content/domain/vo/PostAttachment.java
+++ src/main/java/com/pulse/content/domain/vo/PostAttachment.java
@@ -1,10 +1,9 @@
 package com.pulse.content.domain.vo;
 
+import com.pulse.content.common.enumerate.AttachmentType;
 import com.pulse.content.domain.key.AttachId;
 import com.pulse.content.domain.key.FileId;
 import lombok.*;
-
-import java.util.Objects;
 
 @Getter
 @Builder(access = AccessLevel.PUBLIC)
@@ -24,18 +23,5 @@ public class PostAttachment {
                 .url(url)
                 .fileId(fileId)
                 .build();
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        PostAttachment that = (PostAttachment) o;
-        return Objects.equals(attachId, that.attachId) && Objects.equals(url, that.url) && Objects.equals(fileId, that.fileId);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(attachId, url, fileId);
     }
 }

171fe52c98e0126d99c48c400a94191888b28ec9
dabin0513 <dabin0513@naver.com>
2025-03-03 15:28:45 +0900

    [MP-49] :recycle: PostStatus 필드값 변경

--- src/main/java/com/pulse/content/common/enumerate/PostStatus.java
+++ src/main/java/com/pulse/content/common/enumerate/PostStatus.java
@@ -2,16 +2,17 @@ package com.pulse.content.common.enumerate;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 @Getter
-@AllArgsConstructor
+@RequiredArgsConstructor
 public enum PostStatus {
 
-    PUBLIC("PUBLIC", "공유"),
-    PRIVATE("PRIVATE", "비공개"),
-    DELETED("DELETED", "삭제");
+    DRAFT("DRAFT", "임시 저장"),       // 임시 저장 상태
+    PUBLISHED("PUBLISHED", "게시"),   // 게시가 완료된 상태
+    HIDDEN("HIDDEN", "숨김"),         // 숨김(보관)된 상태
+    DELETED("DELETED", "삭제");       // 삭제된 상태
 
     private final String code;
     private final String description;
-
 }

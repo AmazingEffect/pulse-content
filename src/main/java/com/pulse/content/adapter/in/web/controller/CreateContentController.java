9c4227a1ecdbe5306b1909cbd638c5a8def18c20
dabin0513 <dabin0513@naver.com>
2025-03-03 15:34:11 +0900

    [MP-49] :sparkles: 게시글 작성 상세 로직 작성

--- src/main/java/com/pulse/content/adapter/in/web/controller/CreateContentController.java
+++ src/main/java/com/pulse/content/adapter/in/web/controller/CreateContentController.java
@@ -3,7 +3,7 @@ package com.pulse.content.adapter.in.web.controller;
 import com.pulse.content.adapter.in.web.dto.request.CreateContentRequestDTO;
 import com.pulse.content.adapter.in.web.dto.response.CreateContentResponseDTO;
 import com.pulse.content.adapter.in.web.dto.response.api.ApiResponse;
-import com.pulse.content.application.port.in.outbox.CreateContentsUseCase;
+import com.pulse.content.application.port.in.content.CreateContentsUseCase;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,6 +15,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class CreateContentController {
     private final CreateContentsUseCase createContentsUseCase;
 
+    /**
+     * @apiNote 게시글 등록을 위한 api
+     * @param createContentRequestDTO 게시글 데이터
+     * @return 응답 상태
+     */
     @PostMapping("/v1/create")
     public ResponseEntity<ApiResponse<CreateContentResponseDTO>> createContent(@RequestBody CreateContentRequestDTO createContentRequestDTO) {
         CreateContentResponseDTO createContentResponseDTO = createContentsUseCase.create(createContentRequestDTO);
--- src/main/java/com/pulse/content/application/service/CreateContentService.java
+++ src/main/java/com/pulse/content/application/service/CreateContentService.java
@@ -2,37 +2,63 @@ package com.pulse.content.application.service;
 
 import com.pulse.content.adapter.in.web.dto.request.CreateContentRequestDTO;
 import com.pulse.content.adapter.in.web.dto.response.CreateContentResponseDTO;
-import com.pulse.content.application.port.in.outbox.CreateContentsUseCase;
-import com.pulse.content.application.port.out.CreateContentPort;
+import com.pulse.content.application.port.in.category.FindCategoryUseCase;
+import com.pulse.content.application.port.in.content.CreateContentsUseCase;
+import com.pulse.content.application.port.out.content.CreateContentPort;
+import com.pulse.content.application.port.out.map.CreatePostCategoryMapPort;
 import com.pulse.content.common.annotation.UseCase;
+import com.pulse.content.common.enumerate.PostStatus;
+import com.pulse.content.domain.Category;
 import com.pulse.content.domain.Post;
+import com.pulse.content.domain.map.PostCategoryMap;
 import com.pulse.content.mapper.ContentMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @UseCase
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class CreateContentService implements CreateContentsUseCase {
-    private final CreateContentPort createContentPort;
+
     private final ContentMapper contentMapper;
 
+    private final CreateContentPort createContentPort;
+    private final CreatePostCategoryMapPort createPostCategoryMapPort;
+
+    private final FindCategoryUseCase findCategoryUseCase;
+
+    /**
+     * @apiNote 게시글 작성 및 관련 데이터 저장
+     * -> hashtag, hashtag-map, category-map
+     * @param createContentRequestDto
+     * @return
+     */
     @Transactional
     @Override
     public CreateContentResponseDTO create(CreateContentRequestDTO createContentRequestDto) {
-        Post post = contentMapper.dtoToDomain(createContentRequestDto);
-        // todo: 1. hashTag 저장
-
-        // todo: 2. 게시글 상태(postStatus) 세팅
-
-        // todo: 3. dto -> domain 변환
-
-        // todo: 5. 카테고리 조회 후 추가
-
-        // todo: 4. 게시글 저장
+        // todo: 게시글
+        Post post = contentMapper.createRequestDtoToDomain(createContentRequestDto);
+        // 게시글 상태(postStatus) 세팅
+        post.changePostStatus(PostStatus.PUBLISHED);
+        // 게시글 저장
         Post createdPost = createContentPort.create(post);
 
-        // todo: 5. 첨부 파일 저장(이벤트)
+        // todo: 해시태그
+        // 해시태그 저장
+        // 해시태그 맵 저장
+
+        // todo: 카테고리
+        List<Long> categoryIds = createContentRequestDto.getCategoryIds();
+        // 카테고리 조회
+        List<Category> categories = findCategoryUseCase.findCategoriesByIds(categoryIds);
+        categories.forEach(category -> {
+            // PostCategoryMap 저장
+            PostCategoryMap postCategoryMap = PostCategoryMap.of(createdPost, category);
+            createPostCategoryMapPort.create(postCategoryMap);
+        });
+
         return contentMapper.domainToCreateResponseDTO(createdPost);
     }
 }

1c619e4902022606adafe1f8250d0724d79b1329
dabin0513 <dabin0513@naver.com>
2025-03-04 20:29:29 +0900

    [MP-49] :sparkles: CreatePostHashTagMapUseCase 추가

---
+++ src/main/java/com/pulse/content/application/port/in/map/CreatePostHashTagMapUseCase.java
@@ -0,0 +1,7 @@
+package com.pulse.content.application.port.in.map;
+
+import com.pulse.content.domain.map.PostHashTagMap;
+
+public interface CreatePostHashTagMapUseCase {
+    PostHashTagMap create(PostHashTagMap postHashTagMap);
+}

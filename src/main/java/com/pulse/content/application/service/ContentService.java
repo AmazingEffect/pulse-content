b5c2b90a12ec34a0f337691dc7b326c3b7ebf786
dabin0513 <dabin0513@naver.com>
2025-03-04 21:19:02 +0900

    [MP-49] :recycle: ContentService 수정
    CreateContentService&FindContentService 병합

--- src/main/java/com/pulse/content/application/service/ContentService.java
+++ src/main/java/com/pulse/content/application/service/ContentService.java
@@ -2,10 +2,14 @@ package com.pulse.content.application.service;
 
 import com.pulse.content.adapter.in.web.dto.request.CreateContentRequestDTO;
 import com.pulse.content.adapter.in.web.dto.response.CreateContentResponseDTO;
+import com.pulse.content.adapter.in.web.dto.response.FindContentResponseDTO;
 import com.pulse.content.application.port.in.category.FindCategoryUseCase;
 import com.pulse.content.application.port.in.content.CreateContentsUseCase;
+import com.pulse.content.application.port.in.content.FindContentUseCase;
+import com.pulse.content.application.port.in.hashTag.FindHashTagUseCase;
 import com.pulse.content.application.port.out.HashTag.CreateHashTagPort;
 import com.pulse.content.application.port.out.content.CreateContentPort;
+import com.pulse.content.application.port.out.content.FindContentPort;
 import com.pulse.content.application.port.out.map.CreatePostCategoryMapPort;
 import com.pulse.content.application.port.out.map.CreatePostHashTagMapPort;
 import com.pulse.content.common.annotation.UseCase;
@@ -13,18 +17,20 @@ import com.pulse.content.common.enumerate.PostStatus;
 import com.pulse.content.domain.Category;
 import com.pulse.content.domain.HashTag;
 import com.pulse.content.domain.Post;
+import com.pulse.content.domain.key.PostId;
 import com.pulse.content.domain.map.PostCategoryMap;
 import com.pulse.content.domain.map.PostHashTagMap;
 import com.pulse.content.mapper.ContentMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.ObjectUtils;
 
 import java.util.List;
 
 @UseCase
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
-public class CreateContentService implements CreateContentsUseCase {
+public class ContentService implements CreateContentsUseCase, FindContentUseCase {
 
     private final ContentMapper contentMapper;
 
@@ -33,7 +39,9 @@ public class CreateContentService implements CreateContentsUseCase {
     private final CreatePostCategoryMapPort createPostCategoryMapPort;
     private final CreatePostHashTagMapPort createPostHashTagMapPort;
 
+    private final FindContentPort findContentPort;
     private final FindCategoryUseCase findCategoryUseCase;
+    private final FindHashTagUseCase findHashTagUseCase;
 
     /**
      * @apiNote 게시글 작성 및 관련 데이터 저장
@@ -55,9 +63,14 @@ public class CreateContentService implements CreateContentsUseCase {
         List<String> hashTagNames = createContentRequestDto.getHashTagNames();
 
         hashTagNames.forEach(hashTagName -> {
-            // 해시태그 저장
-            HashTag hashTag = HashTag.of(hashTagName);
-            createHashTagPort.create(hashTag);
+            // 해시태그 조회
+            HashTag hashTag = findHashTagUseCase.findByName(hashTagName);
+
+            // 존재하지 않을 경우 해시태그 저장
+            if (ObjectUtils.isEmpty(hashTag)) {
+                hashTag = HashTag.of(hashTagName);
+                createHashTagPort.create(hashTag);
+            }
 
             // 해시태그 맵 저장
             PostHashTagMap postHashTagMap = PostHashTagMap.of(post, hashTag);
@@ -76,5 +89,11 @@ public class CreateContentService implements CreateContentsUseCase {
         });
 
         return contentMapper.domainToCreateResponseDTO(createdPost);
+    }
+
+    @Override
+    public FindContentResponseDTO findContent(PostId postId) {
+        Post findPost = findContentPort.findContent(postId); // 행위에 대한 이름으로
+        return contentMapper.domainToResponseDTO(findPost);
     }
 }

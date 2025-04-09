5d11629e5d3421128eb84c54a4c6cc53d7a19f79
dabin0513 <dabin0513@naver.com>
2025-04-09 20:59:25 +0900

    [MP-51] :sparkles: ContentHashTagMap 삭제 추가

--- src/main/java/com/pulse/content/application/port/out/map/DeleteContentHashTagMapPort.java
+++ src/main/java/com/pulse/content/application/port/out/map/DeleteContentHashTagMapPort.java
@@ -1,7 +1,11 @@
 package com.pulse.content.application.port.out.map;
 
+import com.pulse.content.domain.key.ContentId;
+
 import java.util.List;
 
 public interface DeleteContentHashTagMapPort {
-    void deleteAll(List<Long> contentHashTagMapIds);
+    void deleteAllById(List<Long> contentHashTagMapIds);
+
+    void deleteAllByContentId(Long contentId);
 }

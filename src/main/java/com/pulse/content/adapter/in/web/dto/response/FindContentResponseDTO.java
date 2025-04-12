29aae9b169418c16daa974f371c57fc6f7059c55
chaesuhyeon <ddd8177@gmail.com>
2025-04-12 13:54:20 +0900

    :sparkles: [MP-43] 게시글 단건 조회에서 게시글-해시태그 map 조회 기능 추가

--- src/main/java/com/pulse/content/adapter/in/web/dto/response/FindContentResponseDTO.java
+++ src/main/java/com/pulse/content/adapter/in/web/dto/response/FindContentResponseDTO.java
@@ -1,14 +1,18 @@
 package com.pulse.content.adapter.in.web.dto.response;
 
 
+import com.pulse.content.domain.HashTag;
 import com.pulse.content.domain.key.ContentId;
 import lombok.Builder;
 import lombok.Getter;
+
+import java.util.List;
 
 @Getter
 @Builder
 public class FindContentResponseDTO {
     private ContentId contentId;
+    private List<String> hashTags;
     private String title;
     private String text;
 }
